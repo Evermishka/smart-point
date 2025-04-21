c82c303c8635ff8e5d686304989778c198678152
Ermishina Evgeniya <evermishka@yandex.ru>
2025-04-21 13:22:27 +0300

    Исправить навигацию на странице Admin

--- frontend/src/pages/admin/Admin.jsx
+++ frontend/src/pages/admin/Admin.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Box, Grid } from '@mui/material';
 import { DrawerMenu } from '../../components';
 import { Navigation } from './components/navigation/Navigation';
 
-export const Admin = () => (
-	<Box sx={{ display: 'flex', flexGrow: 1, flexDirection: 'column' }}>
+export const Admin = () => {
+	const [currentItem, setCurrentItem] = useState(0);
+
+	const handleCurrentItemChange = (itemId) => {
+		setCurrentItem(itemId);
+	};
+
+	return (	<Box sx={{ display: 'flex', flexGrow: 1, flexDirection: 'column' }}>
 		<Grid
 			container
 			spacing={2}
+			direction={{ xs: 'column', md: 'row' }}
 			sx={{ display: 'flex', flexGrow: 1, justifyContent: 'flex-start' }}
 		>
 			<Grid
@@ -17,17 +25,16 @@ export const Admin = () => (
 					gap: 1,
 				}}
 			>
-				<Navigation />
+				<Navigation currentItem={currentItem} handleCurrentItemChange={handleCurrentItemChange} />
 			</Grid>
 			<Grid
 				size={12}
 				sx={{
-					display: { xs: 'flex', md: 'none' },
-					justifyContent: 'space-between',
-					gap: 1,
+					display: { xs: 'flex', md: 'none' }
 				}}
 			>
 				<DrawerMenu openButtonText="Навигация">
+					{(closeDrawer) => <Navigation closeDrawer={closeDrawer} currentItem={currentItem} handleCurrentItemChange={handleCurrentItemChange} />}
 					<Navigation />
 				</DrawerMenu>
 			</Grid>
@@ -43,5 +50,7 @@ export const Admin = () => (
 				<Outlet />
 			</Grid>
 		</Grid>
-	</Box>
-);
+	</Box>)
+
+
+};
--- frontend/src/pages/admin/components/navigation/Navigation.jsx
+++ frontend/src/pages/admin/components/navigation/Navigation.jsx
@@ -1,28 +1,20 @@
 import { List, Stack } from '@mui/material';
 import { NavigationItem } from '../navigation-item/NavigationItem';
 import { ADMIN_NAVIGATION } from '../../../../constants';
-import { useState } from 'react';
 
-export const Navigation = () => {
-	const [currentItem, setCurrentItem] = useState(0);
-
-	const handleCurrentItemChange = (itemId) => {
-		setCurrentItem(itemId);
-	};
-
-	return (
-		<Stack sx={{ justifyContent: 'space-between' }}>
-			<List sx={{ px: 0 }}>
-				{ADMIN_NAVIGATION.map((item) => (
-					<NavigationItem
-						id={item.id}
-						title={item.title}
-						currentItem={currentItem}
-						handleCurrentItemChange={handleCurrentItemChange}
-						route={item.route}
-					/>
-				))}
-			</List>
-		</Stack>
-	);
-};
+export const Navigation = ({ closeDrawer, currentItem, handleCurrentItemChange }) => (
+	<Stack sx={{ justifyContent: 'space-between' }}>
+		<List sx={{ px: 0 }}>
+			{ADMIN_NAVIGATION.map((item) => (
+				<NavigationItem
+					id={item.id}
+					title={item.title}
+					currentItem={currentItem}
+					handleCurrentItemChange={handleCurrentItemChange}
+					route={item.route}
+					onClick={() => closeDrawer()}
+				/>
+			))}
+		</List>
+	</Stack>
+);
